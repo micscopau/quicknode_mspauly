42b95b4b570a16242b8e8f877530f3408648f9de
Michael Pauly <micscopau@gmail.com>
2022-09-17 16:21:48 -0600

    Add initial API request

--- src/components/trending/TrendingContent.tsx
+++ src/components/trending/TrendingContent.tsx
@@ -1,21 +1,40 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { isNullOrUndefined } from "../../utils/functions";
+import { fetchTrending } from "../../utils/requests";
 
 function TrendingContent(): JSX.Element {
   const [ component, setComponent ] = useState<JSX.Element | undefined>(undefined);
+  const [ data, setData ] = useState<any>(undefined);
   const userContext = useContext(UserContext);
   const { userAddress } = userContext;
 
   useEffect( () => {
     if(!isNullOrUndefined(userAddress)){
+      setComponent(<div>Fetching Data</div> );
+
       // fetch trending query results
-      setComponent(<div>Success</div>)
+      // TODO: Update return type.
+      const fetchData = async (): Promise<any> => {
+        setData(await fetchTrending());
+        
+        //TODO: replace/remove this 
+        setComponent(<div>Successfully fetched data</div>);
+      }
+      
+      fetchData().catch( console.error );
     } else {
       setComponent(<div> Please Connect your wallet to view trending collections</div>)
     }
 
-  }, [userAddress])
+  }, [userAddress]);
+
+
+  useEffect( () => {
+    // TODO: properly pass/handle the data (send to a ReactTable component?)
+    console.log("data updated:", data)
+    setComponent(<div>Data has updated... </div>)
+  }, [data])
 
   return (
     <div className="trending-content-wrapper box">
