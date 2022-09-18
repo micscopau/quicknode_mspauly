8a51be8367852ef4f9785e699948d3f3c249cf83
Michael Pauly <micscopau@gmail.com>
2022-09-18 13:27:47 -0600

    Add Table, update TODO's and Readme

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,6 +1,18 @@
-export enum WalletStatuses {
+export enum eWalletStatuses {
   DISCONNECTED = "Disconnected",
   CONNECTED = "Connected",
   MISSING = "Wallet Missing",
   ERROR = "Error Connecting Wallet"
+}
+
+export interface tTrendingData {
+  address: string,
+  name: string,
+  stats: {
+    average: number,
+    ceiling: number,
+    floor: number,
+    totalSales: number,
+    volume: number
+  }
 }
